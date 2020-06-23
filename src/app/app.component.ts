a9e7f2e1e5015727ab97ccb1e65046b3aa5a01b5
Amanda Crepaldi Morettini <amanda.morettini@carambola.com.vc>
2020-06-23 11:03:55 -0300

    Atualização teste com component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '../../node_modules/@angular/forms';
+import { User } from './user';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prjCadeAngular';
+export class AppComponent implements OnInit {
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createUserForm();
+  }
+  sendData() {
+    console.log(this.userForm.value);
+  }
+  createUserForm() {
+    this.userForm = this.fb.group({
+      cpf: [''],
+    });
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User {
+
+    constructor(cpf: string) {
+        this.cpf = cpf;
+    }
+    cpf: string;
+}
