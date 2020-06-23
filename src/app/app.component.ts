9567e6fd446260e0a43c3f1c0f9bfdccf0ecd3a6
Amanda Crepaldi Morettini <amanda.morettini@carambola.com.vc>
2020-06-23 17:14:22 -0300

    UserStory 601 | Task 602

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '../../node_modules/@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '../../node_modules/@angular/forms';
 import { User } from './user';
+import { Validations } from './validations'
 
 
 @Component({
@@ -16,12 +17,21 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.createUserForm();
   }
+  //Vou enviar o CPF para onde? Será que devemos puxar o POST aqui?
   sendData() {
-    console.log(this.userForm.value);
+    console.log(this.userForm.value); 
+    this.userForm.reset();
+
   }
   createUserForm() {
     this.userForm = this.fb.group({
-      cpf: [''],
+      cpf: ['', Validators.compose([Validators.required])],
     });
+  
+  }
+  //Propriedade para obter os erros de validação 
+  get cpf() {
+    return this.userForm.get('cpf');
   }
 }
+
